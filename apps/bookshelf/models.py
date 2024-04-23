01d709289baa5957a9fdd7fb42b47f45917877e9
Jack Barter <jack.w.barter@outlook.com>
2024-04-23 14:46:06 +0100

    style pagination

--- apps/bookshelf/models.py
+++ apps/bookshelf/models.py
@@ -13,6 +13,9 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+        class Meta:
+            ordering = ['title']
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
